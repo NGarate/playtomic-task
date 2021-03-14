ac8a2717d084ac3a4b2d8b70652eba2d66e32e89
ngarate <nouel3k@gmail.com>
2021-03-14 20:48:20 +0100

    Check status on page load

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Redirect,
     Route
 } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useDispatch } from 'react-redux';
+import { checkAuth0Status } from '../store/loginReducer';
 import Dashboard from './Dashboard';
 import Settings from './Settings';
 import SignIn from './SignIn';
 import ProtectedRoute from './common/ProtectedRoute';
 
 export default function MainPage(): JSX.Element {
+    const auth0 = useAuth0();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(checkAuth0Status(auth0));
+    });
+
     return (
         <Router>
             <Switch>
