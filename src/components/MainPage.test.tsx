605eeff86d46e9d3b3dc34eecdf713241d596b03
ngarate <nouel3k@gmail.com>
2021-03-15 04:55:49 +0100

    Use enzyme for snapshots

--- src/components/MainPage.test.tsx
+++ src/components/MainPage.test.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import toJson from 'enzyme-to-json';
 import MainPage from './MainPage';
 import * as redux from 'react-redux';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { shallow, configure } from 'enzyme';
 
 jest.mock('react-redux');
+configure({ adapter: new Adapter() });
 
-describe('Main page', () => {
-    const mockedDispatch = jest.fn();
-    const basicState = {
-        login: {},
-        dashboard: {},
-        settings: {}
+test('Main page renders correctly when user is authenticated', () => {
+    const mockedDispatch = () => {
+        // empty function
     };
-
-    beforeEach(() => {
-        redux.useDispatch.mockImplementation(() => mockedDispatch);
-        redux.useSelector.mockReturnValue(basicState);
+    redux.useSelector.mockReturnValue({
+        isAuthenticated: true,
+        user: { userName: 'userName' }
     });
+    redux.useDispatch.mockImplementation(() => mockedDispatch);
 
-    test('renders correctly', () => {
-        const tree = renderer.create(<MainPage />).toJSON();
-        expect(tree).toMatchSnapshot();
+    const wrapper = shallow(<MainPage />);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+test('Main page renders correctly when user is not authenticated', () => {
+    const mockedDispatch = () => {
+        // empty function
+    };
+    redux.useSelector.mockReturnValue({
+        isAuthenticated: false,
+        user: { userName: 'userName' }
     });
+    redux.useDispatch.mockImplementation(() => mockedDispatch);
+
+    const wrapper = shallow(<MainPage />);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
 });
